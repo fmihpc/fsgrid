cc3f06fd8b07306d22890e15588c85d056b25ba1
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-10-16 14:05:55 +0300

    Add test for local-to-global-to-local coordinate change

--- tests/mpi_tests/grid_tests.cpp
+++ tests/mpi_tests/grid_tests.cpp
@@ -151,3 +151,23 @@ TEST(FsGridTest, compareConstructedFsGridDisplayStringToReference5) {
       ASSERT_EQ(0, refStr.compare(str));
    }
 }
+
+TEST(FsGridTest, localToGlobalRoundtrip1) {
+   const std::array<FsGridTools::FsSize_t, 3> globalSize{1024, 666, 71};
+   const MPI_Comm parentComm = MPI_COMM_WORLD;
+   const std::array<bool, 3> periodic{true, true, false};
+
+   const auto grid = FsGrid<std::array<double, 15>, 1>(globalSize, parentComm, periodic);
+   const auto localSize = grid.getLocalSize();
+   for (int32_t x = 0; x < localSize[0]; x++) {
+      for (int32_t y = 0; y < localSize[1]; y++) {
+         for (int32_t z = 0; z < localSize[2]; z++) {
+            const auto global = grid.localToGlobal(x, y, z);
+            const auto local = grid.globalToLocal(global[0], global[1], global[2]);
+            ASSERT_EQ(local[0], x);
+            ASSERT_EQ(local[1], y);
+            ASSERT_EQ(local[2], z);
+         }
+      }
+   }
+}
